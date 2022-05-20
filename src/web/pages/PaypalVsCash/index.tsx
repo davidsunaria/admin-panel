24111b92f93036c814aa5294a4d211847d5966fb
Praveen Soni <praveen@shinewebservices.com>
2022-05-20 18:48:07 +0530

    paypalcash

--- src/web/pages/PaypalVsCash/index.tsx
+++ src/web/pages/PaypalVsCash/index.tsx
@@ -3,11 +3,13 @@ import { useStoreActions, useStoreState } from "react-app-store";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import LoadingOverlay from "react-loading-overlay-ts";
 import * as _ from "lodash";
-import { toUpperCase} from "../../../lib/utils/Service";
 
 const PaypalVsCash: React.FC = (): JSX.Element => {
   const [paypalCash, setPaypalCash] = useState<[]>([]);
-  const [totalSum, setTotalSum] = useState<any>(null);
+  const [totalSum, setTotalSum] = useState<number>(0);
+  const [payPalAmount, setPayPalAmount] = useState<number>(0);
+  const [cashAmount, setCashAmount] = useState<number>(0);
+  
 
   //State
   const paypalVsCash = useStoreState((state) => state.dashboard.paypalVsCash);
@@ -27,21 +29,34 @@ const PaypalVsCash: React.FC = (): JSX.Element => {
   useEffect(() => {
     getData();
   }, []);
- 
 
   useEffect(() => {
     setPaypalCash(paypalVsCash);
-    let sum = _.sumBy(paypalVsCash, 'total');
-    setTotalSum(sum)
+    let sum = _.sumBy(paypalVsCash, "total");
+    setTotalSum(sum);
+    paypalCash && paypalCash?.length > 0 && paypalCash?.forEach((val:any,i:any)=>{
+      if(val?._id==="cash"){
+        setCashAmount(val?.total)
+      }
+      if(val?._id==="paypal"){
+        setPayPalAmount(val?.total)
+      }
+   })
   }, [paypalVsCash]);
+  
 
+  const getPercentage = useCallback(
+    (perValue: any) => {
+      let response = 0;
+      if(perValue){
+        response = _.round((perValue * 100) / totalSum, 2);
+      }
+      return response;
+    },
+    [paypalCash]
+  );
 
-const getPercentage = useCallback((perValue:any) => {
-  let response =0 
-   response= _.round((perValue*100)/totalSum, 2); 
-   return response;
-  }, [paypalCash]);
-
+  
 
   return (
     <>
@@ -57,17 +72,17 @@ const getPercentage = useCallback((perValue:any) => {
           <div className="dashAppointFilterOuter">
             <div className="dashboardSubTitle">Paypal vs Cash</div>
           </div>
+          
+
           <div className="customScroll">
-             {paypalCash && paypalCash?.length > 0
-              ? paypalCash.map((val:any, i:number) => {
-                  return (
-                    <div key={i} className="detailOuer">
-                      <label className="text-dark">{toUpperCase(val?._id)}</label>
-                     <span>{getPercentage(val?.total)+"%"}</span> 
-                    </div>
-                  );
-                })
-              : null}
+          <div className="detailOuer">
+              <label className="text-dark">Cash</label>
+              <span>{ `${getPercentage(cashAmount)}%`}</span>
+            </div>
+            <div className="detailOuer">
+              <label className="text-dark">Paypal</label>
+              <span>{`${getPercentage(payPalAmount)}%`}</span>
+            </div>
           </div>
         </div>
       </div>
